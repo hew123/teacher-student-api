cf13e1211f59a87d3e252273022c3b04c2774c65
Kieth Hew <keithhew@MacBook-Pro-2.local>
2023-10-08 22:41:05 +0800

    add api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,48 @@
-console.log('hello world')
+import express, { Express, Request, Response } from 'express';
+import bodyParser from 'body-parser';
+
+const app: Express = express();
+// Hardcoded port number as required if undefined
+const port = process.env.PORT || '8080';
+
+// Express 4.x has unbundled middlewares,
+// hence need to explicitly attach JSON body parser
+// https://stackoverflow.com/questions/9177049/express-js-req-body-undefined
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    console.log('Request: ', JSON.stringify(req.path), JSON.stringify(req.body));
+    next()
+})
+
+app.get('/api/commonstudents', (
+    req: Request<undefined, undefined, undefined>, 
+    res: Response<undefined>
+    ) => {
+        res.status(200).json();
+});
+
+app.post('/api/register', (
+    req: Request<undefined, undefined, undefined>, 
+    res: Response<undefined>
+    ) => {
+        res.status(204).json();
+});
+
+app.post('/api/suspend', (
+    req: Request<undefined, undefined, undefined>, 
+    res: Response<undefined>
+    ) => {
+    res.status(204).json();
+});
+
+app.post('/api/retrievefornotifications', (
+    req: Request<undefined, undefined, undefined>, 
+    res: Response<undefined>
+    ) => {
+    res.status(200).json();
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
