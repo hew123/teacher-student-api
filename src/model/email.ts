ff453871277b400c1093e2209c88ca76ce4efb7e
Kieth Hew <keithhew@MacBook-Pro-2.local>
2023-10-10 22:44:53 +0800

    fix regex

--- src/model/email.ts
+++ src/model/email.ts
@@ -1,6 +1,12 @@
 import { RequestError } from "./error";
 
-const EMAIL_REGEX = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/g;
+// 'g' at the back means global search - it is stateful in keeping track of where it leaves off 
+// previously while reading the text - used to extracting multiple email addresses from text
+// BUT should not be used for normal email regex testing because it is stateful
+const EXTRACT_EMAIL_REGEX = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/g;
+// Having another regex without 'g' at the back to validate email address
+const EMAIL_REGEX = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/;
+
 
 // TODO: add test
 export class Email {
@@ -15,7 +21,7 @@ export class Email {
 export class NotificationText {
     emails: Email[]
     constructor(readonly notifyText: string) {
-        const emailStrs = notifyText.match(EMAIL_REGEX);
+        const emailStrs = notifyText.match(EXTRACT_EMAIL_REGEX);
         this.emails = emailStrs?.map((e) => new Email(e)) ?? [];
     }
 }
