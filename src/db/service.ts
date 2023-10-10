b81bf3fb96322e197031c9cd63d28c510604983e
Kieth Hew <keithhew@MacBook-Pro-2.local>
2023-10-10 17:24:12 +0800

    get non suspended students

--- src/db/service.ts
+++ src/db/service.ts
@@ -44,7 +44,6 @@ export class TeacherStudentPersistenceService {
             console.log(`Teacher by ${teacherEmail.id} does not exist. Creating...`);
             teacher = new Teacher(teacherEmail.id);
         }
-
         // TODO: add test 
         // This is to avoid overwriting students already created
         const students = await this.dbConnection.getRepository(Student).findBy({
@@ -79,16 +78,34 @@ export class TeacherStudentPersistenceService {
         })
     }
 
-    async suspend(email: Email): Promise<void> {
+    async suspend(studentEmail: Email): Promise<void> {
         const student = await this.dbConnection.getRepository(Student).findOne({
-            where: { email: email.id }
+            where: { email: studentEmail.id }
         });
         if (student === null) {
-            throw new Error(`Student ${email.id} does not exist.`);
+            throw new Error(`Student ${studentEmail.id} does not exist.`);
         }
 
         student.suspended = true;
         await this.dbConnection.getRepository(Student).save(student);
+    }
+
+    async getNonSuspendedStudents(teacherEmail: Email): Promise<Student[]> {
+        const teacher = await this.dbConnection.getRepository(Teacher).findOne({
+            relations: {
+                students: true,
+            },
+            where: { 
+                email: teacherEmail.id,
+                students: {
+                    suspended: false
+                }
+            }
+        });
+        if (teacher === null) {
+            throw new Error(`Teacher ${teacherEmail} does not exist.`);
+        }
+        return teacher.students;
     }
 }
 
